881d6750eafa79dab3dcb91a1ebbcf2db44f94ee
Christian <christian_lega99@hotmail.com>
2024-04-15 23:09:21 +0200

    Inicio correcto a falta de mas controllers e inicio de sesion

--- src/main/java/com/laguna/supermaket/controller/ProductController.java
+++ src/main/java/com/laguna/supermaket/controller/ProductController.java
@@ -1,7 +1,27 @@
 package com.laguna.supermaket.controller;
 
+import com.laguna.supermaket.persistence.entity.Product;
+import com.laguna.supermaket.service.ProductService;
+import com.laguna.supermaket.service.dto.ProductInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Camino hacia el controlador
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping //Crear elementos
+    public Product createProduct(@RequestBody ProductInDTO productInDTO) {
+        return this.productService.createProduct(productInDTO);
+    }
+
 }
